2f730de918a0357c90816afbb06a85cbffdb2da7
helldzl <1949602@qq.com>
2017-11-07 18:12:43 +0800

    Initial commit

---
+++ moon-sample-algorithm/src/main/java/org/moon/book/algorithm/SampleController.java
@@ -0,0 +1,27 @@
+package org.moon.book.algorithm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @author quzile
+ * @version 1.0
+ * @since 2017/10/28
+ */
+@Controller
+@EnableAutoConfiguration
+public class SampleController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(SampleController.class, args);
+    }
+}
